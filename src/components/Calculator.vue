945f4e8e43c7ff21754dd2e5ab092e9d978bcb30
Victor Abrell <victor@abrell.se>
2020-04-03 17:31:25 +0200

    Initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="calculator">
+
+    <div class="display"></div>
+
+    <div class="buttons">
+
+      <div class="numbers">
+        <button>0</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+      </div>
+
+      <div class="actions"></div>
+
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Calculator'
+}
+</script>
+
+<style scoped>
+</style>
